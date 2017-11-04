45b51a2aefff6fbe533a6cc6e1d46a88973aff23
Ohmo <>
2017-11-04 11:58:16 +0800

    變更狀態

---
+++ admin/all_pack.php
@@ -0,0 +1,21 @@
+<?php 
+include("connMysql.php");
+
+if (!$seldb) die("資料庫選擇失敗");
+
+if (is_array($_POST["id"])) {
+	$id_array = $_POST["id"];
+}else{
+	$id_array = array($_POST["id"]);
+}
+
+foreach ($id_array as $id) {
+	$sql_query  = "UPDATE `pomelo_order` SET ";
+	$sql_query .= "`order_status` = 'pack'";
+
+	mysql_query($sql_query);
+}
+
+echo true;
+
+?>
